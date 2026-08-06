14d7ee3aa29c5ea210f155086645d24097b1cde5
pikobenedek-art <piko.benedek@gmail.com>
2026-08-06 13:31:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,9 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-
+    
+    family: 4,
+    
     auth: {
         user: "lovasi.kertgondozas@gmail.com",
         pass: process.env.EMAIL_JELSZO
